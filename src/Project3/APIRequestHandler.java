fcd2972067747b20afecfd43051609c191887fe6
Camarly <camarly@users.noreply.github.com>
2023-04-10 19:56:48 -0500

    API modification, icon data, cityData HashMap, methods or frequent item etc

--- src/Project3/APIRequestHandler.java
+++ src/Project3/APIRequestHandler.java
@@ -1,59 +1,125 @@
 package Project3;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
+import java.util.*;
 
-import org.json.*;
-
-import static java.util.Collections.frequency;
 import static java.util.Collections.max;
 
 public class APIRequestHandler {
-    private static String cityName = "";
-    private String apiUrl;
-
-    //Camarly's key
-    //public static final String api_KeyCloud = "bf35fb6d7822ade28ea3197bae75439c";
-    //Tevin's key
-    //Michael's key
-
-    //longitude
-    //latitude
-
-    //public static final String POSTS_API_URL = "https://history.openweathermap.org/data/2.5/history/city?lat=18.017874&lon=-76.809906&type=hour&start=1680343200&end=1680386400&appid=" + api_KeyCloud;
-//	public static final String POSTS_API_URL = "https://history.openweathermap.org/data/2.5/history/city?lat=18.017874&lon=76.809906&type=hour&start=1680343200&end=1680735600&appid=ad063a14892951f9896784bfef4481d6";
 
 
+  //  private static final String apiURL =  "https://history.openweathermap.org/data/2.5/aggregated/day?lat=188.47&lon=-77.92&month=4&day=1&appid=dbd8574d10549e41443636960496338d";
 
-    public APIRequestHandler(String apiUrl, String cityName) {
-        this.apiUrl = apiUrl;
-        APIRequestHandler.cityName = cityName;
+    private static final String BASE_API_URL = "http://api.openweathermap.org/geo/1.0/direct?q=%s,JM&limit=1&appid=%s";
+
+    //http://api.openweathermap.org/geo/1.0/direct?q={city name},{country code}&limit={limit}&appid={API key}
+
+   //private String apiURL = "https://history.openweathermap.org/data/2.5/history/city?lat=18.4724603&lon=-77.9217357&type=hour&start=1680343200&end=1680411599&appid=dbd8574d10549e41443636960496338d";
+   //private String apiURLHistoric = "https://history.openweathermap.org/data/2.5/history/city?q=Kingston,JM&type=hour&start=1680343200&end=1680411599&appid=dbd8574d10549e41443636960496338d";
+//
+//    private String apiURLCurrent = "http://api.openweathermap.org/geo/1.0/direct?q=%s,JM&limit=1&appid=%s";
+//    private String apiURLForecast = "http://api.openweathermap.org/geo/1.0/direct?q=%s,JM&limit=1&appid=%s";
+
+    //API Keys
+    private static final String aPiKey01 = "dbd8574d10549e41443636960496338d";
+    private static final String aPiKey02 = "dbd8574d10549e41443636960496338d";
+    private static final String aPiKey03 = "dbd8574d10549e41443636960496338d";
+
+    private String apiURL = "https://history.openweathermap.org/data/2.5/history/city?lat="+ getLatitude() + "&lon="+getLongitude()+"&type=hour&start= " + getStartTime() + "&end=" + getEndTime() + "&appid=" + aPiKey02;
+
+    private int count;
+
+
+    private String longitude;
+    private String latitude;
+
+    //fix city name for output
+    private static String cityName;
+
+    private static String apiResponse;
+
+    private String startTime;
+    private String endTime;
+
+
+    public APIRequestHandler(String cityName, String longitude, String latitude, String startTime, String endTime, String type) {
+        if(type.equals("Historic")) {
+            this.apiURL = "https://history.openweathermap.org/data/2.5/history/city?lat="+ latitude + "&lon="+ longitude+"&type=hour&start=" + startTime + "&end=" + endTime + "&appid=" + aPiKey02+"&units=metric";
+        }
+        else if(type.equals("Current")) {
+            this.apiURL = "https://history.openweathermap.org/data/2.5/history/city?lat="+ latitude + "&lon="+ longitude+"&type=hour&start=" + startTime + "&end=" + endTime + "&appid=" + aPiKey02+"&units=metric";
+        }
+        else {
+            this.apiURL = "https://history.openweathermap.org/data/2.5/history/city?lat="+ latitude + "&lon="+ longitude+"&type=hour&start=" + startTime + "&end=" + endTime + "&appid=" + aPiKey02+"&units=metric";
+        }
+
+
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.cityName = cityName;
     }
 
-    public APIRequestHandler(String WeatherMapApiUrl, City city) {
-        this.apiUrl = WeatherMapApiUrl;
+
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
     }
 
     public String getWeatherData() throws Exception {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiUrl)).build();
 
-        //do not delete
+        //System.out.println(apiURL);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiURL)).build();
+
+            //do not delete
         return (client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenApply(APIRequestHandler::parseWeatherData)
-                .join());
+            .thenApply(HttpResponse::body)
+            .thenApply(APIRequestHandler::parseWeatherData)
+            .join());
 //        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
 //                .thenApply(HttpResponse::body)
-//            .thenAccept(System.out::println)
+//            .thenApply(APIRequestHandler::parseWeatherData)
 //                .join();
     }
-
 
     public static String parseWeatherData(String responseBody) {
 
@@ -70,9 +136,11 @@ public class APIRequestHandler {
         ArrayList<String> modeOfIcon = new ArrayList<>();
 
         JSONObject cityData = new JSONObject(responseBody);
+        //System.out.println(cityData);
 
         JSONObject weatherData = new JSONObject(responseBody);
         JSONArray hourlyReports = weatherData.getJSONArray("list");
+        //System.out.println(weatherData);
 
         for (int i=0; i < hourlyReports.length(); i++) {
             JSONObject report = hourlyReports.getJSONObject(i);
@@ -86,28 +154,86 @@ public class APIRequestHandler {
             icon = report.getJSONArray("weather").getJSONObject(0).getString("icon");
         }
         temp /= count;
+        temp = Math.round(temp * 100.00)/100.00;
         humidity /= count;
         modeOfWeather.add(weather);
         modeOfDesc.add(description);
         modeOfIcon.add(icon);
 
+        weather = getFrequentItem(modeOfWeather);
+        description = getFrequentItem(modeOfWeather);
+        icon = getFrequentItem(modeOfIcon);
 
-
-        //comeback to later
-        //System.out.println("Temperature:" + temp + " , humidity: " + humidity + " Weather: " + max(modeOfWeather));
-
-//        //return null;
-        City aCity =  new City(cityName, temp, humidity, max(modeOfDesc), max(modeOfWeather), max(modeOfIcon), datetime);
+        City aCity =  new City(APIRequestHandler.cityName, temp, humidity, description, weather, icon, datetime);
         City.cityWeatherData.add(aCity);
-//        return null;
-        //return (cityName + " " + temp + " " + humidity + " " + description + " " + type + " " + icon + " " + datetime);
-
-        //returns weather data for city in desired format before creating city class.
 
         return null;
     }
 
 
+
+    //get the most frequent occurring item in collection - code attri : arshajii(stackoverflow)
+    public static <T> T getFrequentItem(ArrayList<T> list) {
+
+            Map<T, Integer> map = new HashMap<>();
+
+            for (T t : list) {
+                Integer val = map.get(t);
+                map.put(t, val == null ? 1 : val + 1);
+            }
+
+            Map.Entry<T, Integer> max = null;
+
+            for (Map.Entry<T, Integer> e : map.entrySet()) {
+                if (max == null || e.getValue() > max.getValue())
+                    max = e;
+            }
+
+        assert max != null;
+        return max.getKey();
+
+    }
+
+
+
+    public static String [] fetchGeoCoordinates(String city){
+
+        try {
+
+            apiResponse = null;
+
+            String query = String.format("%s", URLEncoder.encode(city, "UTF-8"));
+            String apiCall = String.format(BASE_API_URL, query, aPiKey03);
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiCall)).build();
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenAccept(response -> {apiResponse = response;})
+                    .join();
+
+
+            String [] latLon = new String[2];
+            if (apiResponse.equals("[]")){
+                System.out.println("Location not found. Please try another city"); //Employ the use of message boxes
+            } else{
+                JSONArray jArray = new JSONArray(apiResponse);
+                JSONObject results = jArray.getJSONObject(0);
+
+                if (results.getString("country").equals("JM")){
+                    latLon[0] = Double.toString(results.getDouble("lat"));
+                    latLon[1] = Double.toString(results.getDouble("lon"));
+
+                }else
+                    System.out.println("Unable to locate data on this city in Jamaica"); //Employ the use of message boxes
+            }
+            return latLon;
+
+        } catch (UnsupportedEncodingException ueex) {
+            ueex.printStackTrace();
+        }
+        return null;
+    }
 
 
 }
