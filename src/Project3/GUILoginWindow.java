edae11bf1030be137e67d6f602cd555d4ede6e3b
Michael Leighton <michael138leighton@gmail.com>
2023-04-15 21:30:32 -0500

    change login window design

--- src/Project3/GUILoginWindow.java
+++ src/Project3/GUILoginWindow.java
@@ -3,83 +3,101 @@ package Project3;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.ImageIcon;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 import static javax.swing.JOptionPane.ERROR_MESSAGE;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 
-public class GUILoginWindow extends JFrame{
+public class GUILoginWindow extends JFrame {
 
     private final JTextField usernameField;
     private final JPasswordField passwordField;
     private JButton submitButton;
     private JButton clearButton;
     private JButton cancelButton;
-
     private JLabel rainIcon;
     private JPanel loginPanel;
     private JPanel commandsPanel;
     private GUILoginWindow thisFrame;
 
-
     public GUILoginWindow() {
 
         thisFrame = this;
-
         setTitle("Rain - User Login");
 
         JPanel loginPanel = new JPanel();
         JPanel commandsPanel = new JPanel();
+        loginPanel.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+        commandsPanel.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+        loginPanel.setBackground(Color.LIGHT_GRAY);
+        commandsPanel.setBackground(Color.LIGHT_GRAY);
 
+        //JPanel imgContainer = new JPanel(new ImageIcon("./resources/images/frameIcon.png"));
         ImageIcon image = new ImageIcon("./resources/images/frameIcon.png");
-        ImageIcon resizedImage = new ImageIcon(image.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH));
+
+        ImageIcon resizedImage = new ImageIcon(image.getImage().getScaledInstance(150, 200, Image.SCALE_SMOOTH));
         rainIcon = new JLabel(resizedImage);
         rainIcon.setHorizontalAlignment(JLabel.CENTER);
         add(rainIcon, BorderLayout.NORTH);
 
+        // Set font and color for labels and text fields
+        Font labelFont = new Font("Arial", Font.PLAIN, 18);
+        Color labelColor = new Color(52, 73, 94);
+        Font textFieldFont = new Font("Arial", Font.PLAIN, 14);
+        Color textFieldColor = new Color(52, 73, 94);
 
+        JLabel usernameLabel = new JLabel("Username");
+        usernameLabel.setFont(labelFont);
+        usernameLabel.setForeground(labelColor);
+        loginPanel.add(usernameLabel);
 
-        loginPanel.add(new JLabel("Username"));
         usernameField = new JTextField(20);
+        usernameField.setFont(textFieldFont);
+        usernameField.setForeground(textFieldColor);
         loginPanel.add(usernameField);
 
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setFont(labelFont);
+        passwordLabel.setForeground(labelColor);
+        loginPanel.add(passwordLabel);
 
-        loginPanel.add(new JLabel("Password"));
         passwordField = new JPasswordField(20);
+        passwordField.setFont(textFieldFont);
+        passwordField.setForeground(textFieldColor);
         loginPanel.add(passwordField);
 
-
-        loginPanel.setLayout(new GridLayout(2, 3));
+        loginPanel.setLayout(new GridLayout(2, 1));
+        loginPanel.setBorder(BorderFactory.createEmptyBorder(20, 50, 20, 50));
 
         submitButton = new JButton("Submit");
+        submitButton.setFont(labelFont);
+        submitButton.setBackground(new Color(0, 0, 139));
+        submitButton.setForeground(Color.WHITE);
         submitButton.addActionListener(this::loginEvent);
         commandsPanel.add(submitButton);
 
         clearButton = new JButton("Clear");
+        clearButton.setFont(labelFont);
+        clearButton.setBackground(new Color(57, 152, 174));
+        clearButton.setForeground(Color.WHITE);
         clearButton.addActionListener(this::clearEvent);
         commandsPanel.add(clearButton);
 
         cancelButton = new JButton("Cancel");
+        cancelButton.setFont(new Font("Arial", Font.PLAIN, 18));
+        cancelButton.setBackground(new Color(231, 76, 60));
         cancelButton.addActionListener(this::cancelEvent);
         commandsPanel.add(cancelButton);
-
 
         add(loginPanel, BorderLayout.CENTER);
         add(commandsPanel, BorderLayout.SOUTH);
 
-
         pack();
+        setLocationRelativeTo(null);
         setVisible(true);
 
     }
-
 
     public void loginEvent(ActionEvent e) {
         // TODO Auto-generated method stub
